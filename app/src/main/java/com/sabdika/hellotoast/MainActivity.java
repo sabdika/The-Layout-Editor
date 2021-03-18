94f9df213107982195fb24c3b030d341637d934e
sabdika <ghimiresabdika343@gmail.com>
2021-03-18 11:49:29 +0545

    Changed the color of button

--- app/src/main/java/com/sabdika/hellotoast/MainActivity.java
+++ app/src/main/java/com/sabdika/hellotoast/MainActivity.java
@@ -2,8 +2,11 @@ package com.sabdika.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,12 +14,15 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button countButton,button_zero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        countButton = (Button) findViewById(R.id.button_count);
+        button_zero = (Button) findViewById(R.id.button_zero);
     }
 
     public void showToast(View view) {
@@ -24,9 +30,23 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    public void countUp(View view) {
+   public void countUp(View view) {
         ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+       if(mCount%2 !=0){
+           countButton.setBackgroundColor(getResources().getColor(R.color.teal_700));
+           button_zero.setBackgroundColor(getResources().getColor(R.color.pink));
+       }
+       else{
+           countButton.setBackgroundColor(getResources().getColor(R.color.blue));
+       }
+    }
+
+
+    public void zeroBtn(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+        button_zero.setBackgroundColor(getResources().getColor(R.color.grey));
     }
 }
